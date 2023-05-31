513b63728a71ca316bd0ea4b9f48c9fdfb1e3e2e
lajtosrob <lajtosrobert@freemail.hu>
2023-05-31 06:15:21 +0200

    Add project files.

---
+++ Kifejezes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMuveletek
+{
+    public class Kifejezes
+    {
+        int operandusBal;
+        string muvelet;
+        int operandusJobb;
+        private string sor;
+
+        /*
+        public Kifejezes(string scvSor) 
+            : this(int.Parse(scvSor.Split()[0]), scvSor.Split()[1], int.Parse(scvSor.Split()[2]))
+        {
+        }*/
+        
+        public Kifejezes(string scvSor)
+        {
+            var mezok = scvSor.Split();
+            this.operandusBal = int.Parse(mezok[0]);
+            this.muvelet = mezok[1];
+            this.operandusJobb = int.Parse(mezok[2]);
+        }
+        
+        public Kifejezes(int operandusBal, string muvelet, int operandusJobb)
+        {
+            this.operandusBal = operandusBal;
+            this.muvelet = muvelet;
+            this.operandusJobb = operandusJobb;
+        }
+
+        public int OperandusBal { get => operandusBal; }
+        public string Muvelet { get => muvelet; }
+        public int OperandusJobb { get => operandusJobb; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleMuveletek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a)
+            List<Kifejezes> kifejezesek = new List<Kifejezes>();
+
+            var sorok = File.ReadAllLines("kifejezesek.txt");
+
+            foreach (var s in sorok)
+            {
+                var mezok = s.Split();
+                kifejezesek.Add(new Kifejezes(int.Parse(mezok[0]), mezok[1], int.Parse(mezok[2])));
+            }
+            Console.WriteLine("Beolvasás készen van!");
+
+            //b) LINQ
+            List<Kifejezes> kifejezesek2 = File.ReadAllLines("kifejezesek.txt")
+                .Select(sor => new Kifejezes(sor)).ToList(); ;
+
+            //2.
+
+            Console.WriteLine("2. feladat: Kifejezések száma: " + kifejezesek2.Count());
+
+            //3.
+
+            Console.WriteLine(kifejezesek2.Count(x => x.Muvelet(Contains("mod")));
+
+            //4.
+
+
+        }
+    }
+}
